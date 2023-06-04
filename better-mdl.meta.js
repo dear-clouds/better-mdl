fd8203edf6a5c072f5b321ab6b366b983bc9dedb
Mio <mio.bluewatermelon@gmail.com>
2023-06-04 14:12:03 +0700

    Finale 1.3.2

---
+++ better-mdl.meta.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name Better MDL
+// @description An enhance version making the website more friendly & modern
+// @version 1.3.2
+// @author Mio.
+// @supportURL https://mydramalist.com/discussions/general-discussion/88611-gathering-feedbacks
+// @match *://www.mydramalist.com/*
+// @match *mydramalist.com/*
+// @downloadURL https://github.com/dear-clouds/better-mdl/raw/main/better-mdl.user.js
+// @grant GM_addStyle
+// @grant GM.xmlHttpRequest
+// @grant GM_getValue
+// @icon https://github.com/dear-clouds/better-mdl/raw/main/images/favicon.png
+// @namespace https://dear-clouds.carrd.co/#better-mdl
+// @updateURL https://github.com/dear-clouds/better-mdl/raw/main/better-mdl.meta.js
+// ==/UserScript==
