6cc91937bf4bc9b88e09501cf03842d913d3ee89
Mio <mio.bluewatermelon@gmail.com>
2023-08-22 12:13:45 +0700

    Switched to MutationObserver for the search filters

--- src/search/filters.js
+++ src/search/filters.js
@@ -4,7 +4,7 @@ import { token } from '../index.js';
 /*                    Function originally written by Thenia                   */
 /*                  (https://mydramalist.com/profile/Thenia)                  */
 /* -------------------------------------------------------------------------- */
-window.onload = function() {
+
 // Initial statuses for each filter
 let showStatuses = {
   1: null,
@@ -21,11 +21,11 @@ function showTitle(title, status, hasStatus) {
   // When the title has status and the filter for this status is crossed
   if (hasStatus && showStatuses[status] === false) {
     shouldHide = true;
-  } 
+  }
   // When the title has no status and there is any filter checked
   else if (!hasStatus && Object.values(showStatuses).some(val => val === true)) {
     shouldHide = true;
-  } 
+  }
   // When the title has status and there is no filter checked for this status and no filter is crossed for this status
   else if (hasStatus && showStatuses[status] !== true && showStatuses[status] !== false) {
     // When there are filters checked for other statuses, hide the title
@@ -48,164 +48,113 @@ function restoreAllTitles() {
   });
 }
 
-// Create the new filter box
-const newFilterBox = document.createElement('div');
-newFilterBox.className = 'filter-box';
+function runScript() {
+  // Create the new filter box
+  const newFilterBox = document.createElement('div');
+  newFilterBox.className = 'filter-box';
 
-// Create the filter header
-const filterHeader = document.createElement('div');
-filterHeader.className = 'filter-header';
-filterHeader.innerHTML = '🤍 <b>BetterMDL</b>';
+  // Create the filter header
+  const filterHeader = document.createElement('div');
+  filterHeader.className = 'filter-header';
+  filterHeader.innerHTML = '🤍 <b>BetterMDL</b>';
 
-// Create the additional information
-const additionalInfo = document.createElement('div');
-additionalInfo.className = 'text-xs text-muted';
-additionalInfo.innerHTML = 'Recheck filters on each page/refresh.';
-filterHeader.appendChild(additionalInfo);
+  // Create the additional information
+  const additionalInfo = document.createElement('div');
+  additionalInfo.className = 'text-xs text-muted';
+  additionalInfo.innerHTML = 'Recheck filters on each page/refresh.';
+  filterHeader.appendChild(additionalInfo);
 
-// Create the filter body
-const filterBody = document.createElement('div');
-filterBody.className = 'filter-body row';
+  // Create the filter body
+  const filterBody = document.createElement('div');
+  filterBody.className = 'filter-body row';
 
-// Create a filter for each status
-const statuses = {
-  1: 'Currently Watching',
-  2: 'Completed',
-  3: 'Plan to Watch',
-  4: 'On Hold',
-  5: 'Dropped',
-  6: 'Not Interested'
-};
+  // Create a filter for each status
+  const statuses = {
+    1: 'Currently Watching',
+    2: 'Completed',
+    3: 'Plan to Watch',
+    4: 'On Hold',
+    5: 'Dropped',
+    6: 'Not Interested'
+  };
 
-// Retrieve title IDs from the page
-const titleElements = document.querySelectorAll('.box[id^="mdl-"]');
-const titleIds = Array.from(titleElements).map(element => element.id.split('-')[1]);
+  // Retrieve title IDs from the page
+  const titleElements = document.querySelectorAll('.box[id^="mdl-"]');
+  const titleIds = Array.from(titleElements).map(element => element.id.split('-')[1]);
 
-// Construct the URL for the API call
-const baseUrl = 'https://mydramalist.com/v1/users/data';
-const params = new URLSearchParams({
-  lang: 'en-US',
-  token: token,
-  mylist: titleIds.join('-')
-});
-const apiUrl = `${baseUrl}?${params.toString()}`;
-
-for (let status in statuses) {
-  // Create the checkbox
-  const checkbox = document.createElement('input');
-  checkbox.type = 'checkbox';
-  checkbox.id = `hideStatus${status}Checkbox`;
-  checkbox.name = `hideStatus${status}Checkbox`;
-  checkbox.value = `hideStatus${status}`;
-  checkbox.style.display = 'none';
-
-  const label = document.createElement('label');
-  label.className = 'filter-select with-exclude col-xs-6 col-sm-4 col-md-6 col-lg-6';
-  label.htmlFor = checkbox.id;
-
-  const checkedIcon = document.createElement('i');
-  checkedIcon.className = 'far text-primary include';
-  checkedIcon.style.display = 'none';
-  checkedIcon.style.marginRight = '5px';
-  label.appendChild(checkedIcon);
-
-  const uncheckedIcon = document.createElement('i');
-  uncheckedIcon.className = 'far fa-square text-primary';
-  uncheckedIcon.style.marginRight = '5px';
-  label.appendChild(uncheckedIcon);
-
-  // Additional icon for the 'crossed' state
-  const crossedIcon = document.createElement('i');
-  crossedIcon.className = 'far text-primary exclude';
-  crossedIcon.style.display = 'none';
-  crossedIcon.style.marginRight = '5px';
-  label.appendChild(crossedIcon);
-
-  const labelText = document.createTextNode(statuses[status]);
-  label.appendChild(labelText);
-
-  label.addEventListener('click', function (e) {
-    e.preventDefault();
-    e.stopPropagation();
-    // Toggle state logic
-    if (showStatuses[status] === null) {
-      showStatuses[status] = true;
-      checkedIcon.style.display = 'inline';
-      uncheckedIcon.style.display = 'none';
-      crossedIcon.style.display = 'none';
-    } else if (showStatuses[status] === true) {
-      showStatuses[status] = false;
-      checkedIcon.style.display = 'none';
-      uncheckedIcon.style.display = 'none';
-      crossedIcon.style.display = 'inline';
-    } else {
-      showStatuses[status] = null;
-      checkedIcon.style.display = 'none';
-      uncheckedIcon.style.display = 'inline';
-      crossedIcon.style.display = 'none';
-    }
-
-    // If no filter is checked or crossed, restore all titles
-    if (!Object.values(showStatuses).some(val => val !== null)) {
-      restoreAllTitles();
-      return;
-    }
-
-    // Make a single API call using the constructed URL
-    $.getJSON(apiUrl, function (json) {
-      // Process the response
-      const titles = document.querySelectorAll('.text-primary.title');
-      titles.forEach(title => {
-        const titleId = title.querySelector('.btn-manage-list').getAttribute('data-id');
-        const movie = json.mylist.find(item => item.rid.toString() === titleId);
-        const hasStatus = !!movie;
-        showTitle(title, movie?.status, hasStatus);
-      });
-    });
+  // Construct the URL for the API call
+  const baseUrl = 'https://mydramalist.com/v1/users/data';
+  const params = new URLSearchParams({
+    lang: 'en-US',
+    token: token,
+    mylist: titleIds.join('-')
   });
+  const apiUrl = `${baseUrl}?${params.toString()}`;
 
-  filterBody.appendChild(label);
-  filterBody.appendChild(checkbox);
-}
+  for (let status in statuses) {
+    // Create the checkbox
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.id = `hideStatus${status}Checkbox`;
+    checkbox.name = `hideStatus${status}Checkbox`;
+    checkbox.value = `hideStatus${status}`;
+    checkbox.style.display = 'none';
 
-// Add the filter header and filter body to the new filter box
-newFilterBox.appendChild(filterHeader);
-newFilterBox.appendChild(filterBody);
+    const label = document.createElement('label');
+    label.className = 'filter-select with-exclude col-xs-6 col-sm-4 col-md-6 col-lg-6';
+    label.htmlFor = checkbox.id;
 
-// Add the new filter box after the 'Type' filter box
-const existingFilterBox = document.querySelector('.filter-box');
-existingFilterBox.parentNode.insertBefore(newFilterBox, existingFilterBox.nextSibling);
+    const checkedIcon = document.createElement('i');
+    checkedIcon.className = 'far text-primary include';
+    checkedIcon.style.display = 'none';
+    checkedIcon.style.marginRight = '5px';
+    label.appendChild(checkedIcon);
 
-// Add an event listener to the pagination links
-document.querySelector('.pagination').addEventListener('click', (e) => {
-  if(e.target.tagName === 'A') { // If clicked target is a link
-    setTimeout(() => {
-      // Reset statuses
-      showStatuses = {
-        1: null,
-        2: null,
-        3: null,
-        4: null,
-        5: null,
-        6: null
-      };
+    const uncheckedIcon = document.createElement('i');
+    uncheckedIcon.className = 'far fa-square text-primary';
+    uncheckedIcon.style.marginRight = '5px';
+    label.appendChild(uncheckedIcon);
 
-      restoreAllTitles();
+    // Additional icon for the 'crossed' state
+    const crossedIcon = document.createElement('i');
+    crossedIcon.className = 'far text-primary exclude';
+    crossedIcon.style.display = 'none';
+    crossedIcon.style.marginRight = '5px';
+    label.appendChild(crossedIcon);
 
-      // Clear the checkboxes
-      for (let status in statuses) {
-        document.querySelector(`#hideStatus${status}Checkbox`).checked = false;
+    const labelText = document.createTextNode(statuses[status]);
+    label.appendChild(labelText);
+
+    label.addEventListener('click', function (e) {
+      e.preventDefault();
+      e.stopPropagation();
+      // Toggle state logic
+      if (showStatuses[status] === null) {
+        showStatuses[status] = true;
+        checkedIcon.style.display = 'inline';
+        uncheckedIcon.style.display = 'none';
+        crossedIcon.style.display = 'none';
+      } else if (showStatuses[status] === true) {
+        showStatuses[status] = false;
+        checkedIcon.style.display = 'none';
+        uncheckedIcon.style.display = 'none';
+        crossedIcon.style.display = 'inline';
+      } else {
+        showStatuses[status] = null;
+        checkedIcon.style.display = 'none';
+        uncheckedIcon.style.display = 'inline';
+        crossedIcon.style.display = 'none';
       }
 
-      // Re-run script
-      const titleElements = document.querySelectorAll('.box[id^="mdl-"]');
-      const titleIds = Array.from(titleElements).map(element => element.id.split('-')[1]);
-      const params = new URLSearchParams({
-        lang: 'en-US',
-        mylist: titleIds.join('-')
-      });
-      const apiUrl = `${baseUrl}?${params.toString()}`;
+      // If no filter is checked or crossed, restore all titles
+      if (!Object.values(showStatuses).some(val => val !== null)) {
+        restoreAllTitles();
+        return;
+      }
+
+      // Make a single API call using the constructed URL
       $.getJSON(apiUrl, function (json) {
+        // Process the response
         const titles = document.querySelectorAll('.text-primary.title');
         titles.forEach(title => {
           const titleId = title.querySelector('.btn-manage-list').getAttribute('data-id');
@@ -214,10 +163,98 @@ document.querySelector('.pagination').addEventListener('click', (e) => {
           showTitle(title, movie?.status, hasStatus);
         });
       });
-    }, 1000); // Wait 1s for the content to load
+    });
+
+    filterBody.appendChild(label);
+    filterBody.appendChild(checkbox);
   }
+
+  // Add the filter header and filter body to the new filter box
+  newFilterBox.appendChild(filterHeader);
+  newFilterBox.appendChild(filterBody);
+
+  // Add the new filter box after the 'Type' filter box
+  const existingFilterBox = document.querySelector('.filter-box');
+  existingFilterBox.parentNode.insertBefore(newFilterBox, existingFilterBox.nextSibling);
+
+  // Add an event listener to the pagination links
+  document.querySelector('.pagination').addEventListener('click', (e) => {
+    if (e.target.tagName === 'A') { // If clicked target is a link
+      setTimeout(() => {
+        // Reset statuses
+        showStatuses = {
+          1: null,
+          2: null,
+          3: null,
+          4: null,
+          5: null,
+          6: null
+        };
+
+        restoreAllTitles();
+
+        // Clear the checkboxes
+        for (let status in statuses) {
+          document.querySelector(`#hideStatus${status}Checkbox`).checked = false;
+        }
+
+        // Re-run script
+        const titleElements = document.querySelectorAll('.box[id^="mdl-"]');
+        const titleIds = Array.from(titleElements).map(element => element.id.split('-')[1]);
+        const params = new URLSearchParams({
+          lang: 'en-US',
+          mylist: titleIds.join('-')
+        });
+        const apiUrl = `${baseUrl}?${params.toString()}`;
+        $.getJSON(apiUrl, function (json) {
+          const titles = document.querySelectorAll('.text-primary.title');
+          titles.forEach(title => {
+            const titleId = title.querySelector('.btn-manage-list').getAttribute('data-id');
+            const movie = json.mylist.find(item => item.rid.toString() === titleId);
+            const hasStatus = !!movie;
+            showTitle(title, movie?.status, hasStatus);
+          });
+        });
+      }, 1000); // Wait 1s for the content to load
+    }
+  });
+
+}
+
+// Checks if the page is already loaded and, if so, runs the script immediately
+if (document.readyState === 'loading') {
+  document.addEventListener('DOMContentLoaded', runScript);
+} else {
+  runScript();
+}
+
+const observer = new MutationObserver(mutations => {
+  for (let mutation of mutations) {
+      if (mutation.type === 'childList') {
+          // Check added nodes
+          for (let node of mutation.addedNodes) {
+              if (node.nodeType === Node.ELEMENT_NODE && 
+                  Array.from(node.classList).some(className => className.startsWith('mdl-'))) {
+                  runScript();
+              }
+          }
+          
+          // Check removed nodes
+          for (let node of mutation.removedNodes) {
+              if (node.nodeType === Node.ELEMENT_NODE && 
+                  Array.from(node.classList).some(className => className.startsWith('mdl-'))) {
+              }
+          }
+      }
+  }
+});
+
+observer.observe(document.body, {
+  childList: true,
+  subtree: true // ensures observing all descendants too
 });
 
 
 
-}
+
+
